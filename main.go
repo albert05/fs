849752a22b532b2f2f606b744a5053796aa892c1
albert <fengelom@qq.com>
2018-12-01 15:47:53 +0800

    deny access dir

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"fmt"
+	"path/filepath"
 )
 
 var BaseConfig map[string]string
@@ -23,13 +24,36 @@ func main() {
 	for _, prefix := range prefixList {
 		p := fmt.Sprintf("/%s/", prefix)
 		path := fmt.Sprintf("%s/%s", BaseConfig["ROOT_PATH"], prefix)
-		http.Handle(p, http.StripPrefix(p, http.FileServer(http.Dir(path))))
+		http.Handle(p, http.StripPrefix(p, filterDir(http.FileServer(http.Dir(path)))))
 	}
 
 	err := http.ListenAndServe(":8899", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func filterDir(h http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !isAllow(r.URL.Path) {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func isAllow(path string) bool {
+	fileTypeList := strings.Split(BaseConfig["ALLOW_FILE_TYPES"], ",")
+	ext := strings.TrimLeft(filepath.Ext(filepath.Base(strings.TrimRight(path, "/"))), ".")
+
+	for _, t := range fileTypeList {
+		if ext == t {
+			return true
+		}
+	}
+
+	return false
 }
 
 func initConfig(file string) map[string]string {
